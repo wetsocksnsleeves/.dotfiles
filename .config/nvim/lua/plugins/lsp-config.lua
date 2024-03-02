ce93bad16991e903824f8ac43fbd65c9b4b9ca90
Etao Tsai <etao@Etaos-MacBook-Air.local>
2024-03-03 00:44:06 +1000

    nvim optimisation + alacritty setup

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,36 +1,41 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "clang-format", "pyright", "mypy", "pylint" },
+				ensure_installed = { "pyright" },
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
-        lazy = false,
+		lazy = false,
 		config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
+				capabilities = capabilities,
+			})
 			lspconfig.clangd.setup({
 				capabilities = capabilities,
 				cmd = { "clangd", "--clang-tidy" },
 			})
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-                filetypes = {"python"}
-            })
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				filetypes = { "python" },
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
