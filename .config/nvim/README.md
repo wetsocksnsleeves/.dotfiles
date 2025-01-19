94e35675a1e533dfae517129f8d41a2e33ee80f8
realecto <ectos69@gmail.com>
2025-01-19 19:53:29 +1000

    Removed un-used nvim plugins

---
+++ .config/nvim/README.md
@@ -0,0 +1,6 @@
+**Lsp Config**  
+- Visit the documentations to get detailed installation and configurations for different lsps
+\
+Some main ones include:
+- Use yarn to install pyright. Mason has this weird issue with pyenv.
+- tailwindcss lsp is installed via yarn.
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -12,24 +12,7 @@ vim.wo.relativenumber=true
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
 
--- Useful remaps
+-- Useful remaps --
 vim.keymap.set('n', "<F5>", ":!python3 %<CR>")
 vim.keymap.set('n', "<leader>fx", ":!chmod +x %<CR>")
 vim.keymap.set('n', '<leader>fr', ':%s//g<left><left>', { noremap = true })
-
--- Compiler.nvim keymaps (Doesn't work unless in here for some reason)
-
--- Open compiler
-vim.keymap.set("n", "<F6>", ":CompilerOpen<CR>", { noremap = true, silent = true })
-
--- Redo last selected option
-vim.keymap.set(
-	"n",
-	"<S-F6>",
-	"<cmd>CompilerStop<cr>" -- (Optional, to dispose all tasks before redo)
-		.. "<cmd>CompilerRedo<cr>",
-	{ noremap = true, silent = true }
-)
-
--- Toggle compiler results
-vim.keymap.set("n", "<S-F7>", "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
