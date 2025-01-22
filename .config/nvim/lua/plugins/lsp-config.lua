55869cd6d030c0c071fc158e054dd647d45d782f
realecto <ectos69@gmail.com>
2025-01-22 22:26:47 +1000

    Added format on file save feature

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,59 +1,60 @@
 return {
-	{
-		"williamboman/mason.nvim", lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local cmp_nvim_lsp = require("cmp_nvim_lsp")
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				{},
-				vim.lsp.protocol.make_client_capabilities(),
-				cmp_nvim_lsp.default_capabilities()
-			)
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            -- Setups
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                vim.lsp.protocol.make_client_capabilities(),
+                cmp_nvim_lsp.default_capabilities()
+            )
 
-			local lspconfig = require("lspconfig")
-			lspconfig.denols.setup({})
+            local lspconfig = require("lspconfig")
+            lspconfig.denols.setup({})
 
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-				cmd = { "clangd", "--clang-tidy" },
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ltex.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.denols.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.tailwindcss.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
+                cmd = { "clangd", "--clang-tidy" },
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.ltex.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.denols.setup({
+                capabilities = capabilities,
+            })
 
-			-- Key bindings
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
-		end,
-	},
+            -- Key bindings
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+        end,
+    },
 }
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -28,9 +28,17 @@ return {
             autostart = true,
         })
 
+        -- Key binds
         vim.keymap.set("n", "<leader>gf", function()
             print("File formatted")
             vim.lsp.buf.format()
-        end, {})
+        end)
+
+        -- Format on write
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            callback = function()
+                vim.lsp.buf.format()
+            end,
+        })
     end,
 }
