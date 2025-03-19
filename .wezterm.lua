5dc7c5e147c7f3642afb5d27de496e2fea01cc8a
realecto <ectos69@gmail.com>
2025-03-20 00:20:33 +1000

    Added wezterm config for windows

---
+++ .wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.color_scheme = 'Batman'
+
+config.font = wezterm.font { family = "JetBrainsMono Nerd Font"}
+config.font_size = 12.0
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.initial_rows = 43
+config.initial_cols = 187
+config.window_background_opacity = 0.88
+
+-- Special
+config.default_prog = { 'C:\\Windows\\System32\\wsl.exe', '--cd', '~'}
+wezterm.on("gui-startup", function(command_line)
+  local tab, pane, window = wezterm.mux.spawn_window(command_line or {})
+  window:gui_window():set_position(15, 15) -- Example: position at (100, 100)
+end)
+
+-- and finally, return the configuration to wezterm
+return config
