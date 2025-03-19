cb9d6709154f7867639c9869fdb974ddd233ad14
realecto <ectos69@gmail.com>
2025-03-20 00:35:36 +1000

    Changed color scheme to Kanagawa Dragon. Swapped to using Acrylic bg

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.color_scheme = 'Batman'
+config.color_scheme = 'Kanagawa Dragon (Gogh)'
 
 config.font = wezterm.font { family = "JetBrainsMono Nerd Font"}
 config.font_size = 12.0
@@ -13,7 +13,8 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.initial_rows = 43
 config.initial_cols = 187
-config.window_background_opacity = 0.88
+config.window_background_opacity = 0.8
+config.win32_system_backdrop = "Acrylic"
 
 -- Special
 config.default_prog = { 'C:\\Windows\\System32\\wsl.exe', '--cd', '~'}
