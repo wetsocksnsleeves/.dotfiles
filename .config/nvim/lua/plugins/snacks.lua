f47899d5eead6b28de3794dabf5ac27ce2e4b12d
Etao <realecto@users.noreply.github.com>
2025-08-22 12:49:22 +1000

    Added snacks and changed telescope config

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        bigfile = { enabled = true },
+        indent = { enabled = true },
+        input = { enabled = true },
+        -- Using telescope for this
+        -- picker = { enabled = true },
+        notifier = { enabled = true },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        words = { enbaled = true }
+    },
+}
