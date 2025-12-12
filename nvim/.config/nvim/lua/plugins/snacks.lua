119c88a521d81ebd1986757c9555f589989d0f46
wetsocksnsleeves <ectos69@gmail.com>
2025-12-12 23:54:03 +1000

    feat: personalised nvim homescreen

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -4,9 +4,6 @@ return {
     lazy = false,
     ---@type snacks.Config
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
         bigfile = { enabled = true },
         indent = { enabled = true },
         input = { enabled = true },
