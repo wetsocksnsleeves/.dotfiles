7d3869f3c9a68f5066512246dbbabeee716838cc
wetsocksnsleeves <ectos69@gmail.com>
2025-12-13 11:23:36 +1000

    feat: Changed terminal colors, added some visual and qol plugins

---
+++ nvim/.config/nvim/lua/plugins/visual.lua
@@ -0,0 +1,11 @@
+return {
+    "sphamba/smear-cursor.nvim",
+    config = function()
+        require("smear_cursor").setup({
+            cursor_color = "#ffffff",
+            stifness = 0.8,
+            trailing_stiffness = 0.5,
+            distance_stop_animating = 0.5
+        })
+    end
+}
