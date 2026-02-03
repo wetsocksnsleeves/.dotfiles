69941e8cd1c42b2ac4a7fde06ad26676a2183f91
etao <etao@ectom1.local>
2026-02-03 22:52:13 +1000

    fix: treesitter issues causing auto tags to not work

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,15 +2,15 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter").setup({
-            ensure_installed = { "lua", "javascript", "typescript", "tsx" },
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "lua", "javascript", "typescript", "tsx", "html", "embedded_template" },
             highlight = { enable = true },
             auto_install = true,
-            autotag = { enable = true },
             indent = { enable = true },
         })
     end,
     dependencies = {
         'nvim-treesitter/nvim-treesitter-context',
-    }
+        "windwp/nvim-ts-autotag"
+    },
 }
