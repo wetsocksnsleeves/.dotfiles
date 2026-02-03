314d1f76232593a1799e1537a465e355f825c74f
etao <etao@ectom1.local>
2026-02-03 22:34:37 +1000

    feat: Update lsp to point to mise shims

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,10 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
     vim.lsp.config("ruby_lsp", {
-        cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") },
+        cmd = { vim.fn.expand("~/.local/share/mise/shims/ruby-lsp") },
     }),
     vim.lsp.config("sorbet", {
-        cmd = { vim.fn.expand("~/.rbenv/shims/srb"), "tc", "--lsp", "--disable-watchman" },
+        cmd = { vim.fn.expand("~/.local/share/mise/shims/srb"), "tc", "--lsp", "--disable-watchman" },
     }),
     vim.lsp.config("rubocop", {
         enabled = false,
