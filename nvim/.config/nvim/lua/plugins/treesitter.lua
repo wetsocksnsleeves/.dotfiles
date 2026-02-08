dd0e13f23b44bbdd6b31a98e581c4c4352919de1
wetsocksnsleeves <ectos69@gmail.com>
2026-02-09 08:53:32 +1000

    fix: treesitter setup

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,12 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "lua", "javascript", "typescript", "tsx", "html", "embedded_template" },
-            highlight = { enable = true },
-            auto_install = true,
-            indent = { enable = true },
-        })
-    end,
+    opts = {
+        ensure_installed = { "lua", "javascript", "typescript", "tsx", "html", "embedded_template" },
+        highlight = { enable = true },
+        auto_install = true,
+        indent = { enable = true },
+    },
     dependencies = {
         'nvim-treesitter/nvim-treesitter-context',
         "windwp/nvim-ts-autotag"
